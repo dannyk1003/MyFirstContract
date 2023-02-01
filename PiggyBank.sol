8a9a7497f84352e01537f59eabeb87ff1060e3a9
Danny-NB <1003danny@gmail.com>
2023-02-01 17:24:11 +0800

    onlyOwner

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.17;
 
 contract PiggyBank {
-    constructor() payable {}
+    address public owner;
+    modifier onlyOwner() {
+        require(owner == msg.sender, "only owner can call this function");
+        _;
+    }
+    constructor() payable {
+        owner = msg.sender;
+    }
     receive() payable external {}
-    function withdraw() external {
+    function withdraw() onlyOwner external {
         address payable Receiver = payable(msg.sender);
        Receiver.transfer(address(this).balance); 
     }
