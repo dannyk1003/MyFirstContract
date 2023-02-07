412bf662209d1e345395898729f09b7917aaf139
Danny-NB <1003danny@gmail.com>
2023-02-07 10:48:21 +0800

    event

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -3,16 +3,24 @@ pragma solidity ^0.8.17;
 
 contract PiggyBank {
     address public owner;
+    event Create (address owner, uint256 value);
+    event Receive (address indexed sender, uint256 value);
+    event Withdraw (address indexed owner, uint256 indexed value);
     modifier onlyOwner() {
         require(owner == msg.sender, "only owner can call this function");
         _;
     }
     constructor() payable {
         owner = msg.sender;
+        emit Create(owner, msg.value);
     }
-    receive() payable external {}
+    receive() payable external {
+        emit Receive(msg.sender, msg.value);
+    }
     function withdraw() onlyOwner external {
         address payable Receiver = payable(msg.sender);
-       Receiver.transfer(address(this).balance); 
+        uint256 value = address(this).balance;
+        Receiver.transfer(value);
+        emit Withdraw(Receiver, value);
     }
 }
